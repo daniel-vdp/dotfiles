b09f10608b9b11d832fb57d88806049becd1d58a
Daniel van der Ploeg <me@danielvdp.com>
2026-06-09 09:27:37 +0930

    fix: load nvm path on startup when node not on PATH

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,19 @@
+-- Load nvm if node is not on PATH
+if vim.fn.executable("node") == 0 then
+  local nvm_dir = os.getenv("NVM_DIR") or (os.getenv("HOME") .. "/.nvm")
+  local nvm_sh = nvm_dir .. "/nvm.sh"
+  if vim.fn.filereadable(nvm_sh) == 1 then
+    local path = vim.fn.system(string.format(
+      'bash -c \'. "%s" && nvm use default --silent >/dev/null 2>&1 && echo "$PATH"\'',
+      nvm_sh
+    ))
+    path = path:gsub("\n", "")
+    if path ~= "" then
+      vim.env.PATH = path
+    end
+  end
+end
+
 require("daniel.settings")
 require("daniel.keymap")
 require("daniel.autocmd")
